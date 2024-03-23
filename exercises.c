cd829142086ef8b9adfcdb2f3b090242eb49e537
Your Name <you@example.com>
2024-03-23 22:55:53 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -94,14 +94,13 @@ Puedes usar una pila auxiliar.
 
 void copia_pila(Stack* P1, Stack* P2) {
    Stack* PilaAux = create_stack();
-   int *elemento;
     while(top(P1) != NULL){
-      *elemento = top(P1);
+      int *elemento = top(P1);
       push(PilaAux, elemento);
       pop(P1);
     }
     while(top(PilaAux) != NULL){
-      *elemento = top(PilaAux);
+      int *elemento = top(PilaAux);
       push(P1, elemento);
       push(P2, elemento);
       pop(PilaAux);
